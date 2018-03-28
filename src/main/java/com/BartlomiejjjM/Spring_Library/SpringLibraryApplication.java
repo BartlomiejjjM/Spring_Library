01a6e86d77e7e6048c982c9436e45bbe9c94b12e
Bartlomiej Moszynski <bartlomiejmoszynski@gmail.com>
2018-03-28 13:00:55 +0200

    Initial commit

---
+++ src/main/java/com/BartlomiejjjM/Spring_Library/SpringLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.BartlomiejjjM.Spring_Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryApplication.class, args);
+	}
+}
